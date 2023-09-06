4c508c129f645121692373affcfe0ad985958500
rabeea-abo-gnim <rabeea06072006@gmail.com>
2023-09-06 18:40:20 +0300

    Update servir.js

--- servir.js
+++ servir.js
@@ -1,6 +1,6 @@
 const http =require("http");
 const app =require("./App")
-const port =7700;//local
+const port =  process.env.port ||7700;//local
 const server=http.createServer(app);
 module.exports=server;
 app.listen(port);
